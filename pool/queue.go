6e0109d83b4478b332fa4becd6ce6fc725c4550b
James Jarvis <git@jamesjarvis.io>
2022-05-28 22:29:15 +0100

    add queue implementation

---
+++ pool/queue.go
@@ -0,0 +1,58 @@
+package pool
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+// queue handles access into and out of the internal queue.
+type queue struct {
+	ch chan UnitOfWork
+
+	waitClose *sync.WaitGroup
+}
+
+func (q *queue) enqueue(ctx context.Context, e UnitOfWork) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case q.ch <- e:
+		return nil
+	}
+}
+
+func (q *queue) dequeue() (UnitOfWork, bool) {
+	select {
+	case v, ok := <-q.ch:
+		return v, ok
+	default:
+		return nil, false
+	}
+}
+
+func (q *queue) size() int {
+	return len(q.ch)
+}
+
+func (q *queue) cap() int {
+	return cap(q.ch)
+}
+
+func (q *queue) close() {
+	close(q.ch)
+	for {
+		if q.size() == 0 {
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+	q.waitClose.Done()
+}
+
+func newQueue(bufferSize int, waitClose *sync.WaitGroup) *queue {
+	return &queue{
+		ch:        make(chan UnitOfWork, bufferSize),
+		waitClose: waitClose,
+	}
+}
