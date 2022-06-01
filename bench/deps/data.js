074ffb39388ae9c061335a38317db7b4dec96198
github-action-benchmark <github@users.noreply.github.com>
2022-06-01 19:15:41 +0000

    add Benchmark (go) benchmark result for 05b80c2fd6b17849d4b5bb1882a3da4ab32959b2

--- bench/deps/data.js
+++ bench/deps/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654110586924,
+  "lastUpdate": 1654110941552,
   "repoUrl": "https://github.com/jamesjarvis/massivelyconcurrentsystems",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 19913643,
             "unit": "ns/op\t      12 B/op\t       0 allocs/op",
             "extra": "584 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@jamesjarvis.io",
+            "name": "James Jarvis",
+            "username": "jamesjarvis"
+          },
+          "committer": {
+            "email": "git@jamesjarvis.io",
+            "name": "James Jarvis",
+            "username": "jamesjarvis"
+          },
+          "distinct": true,
+          "id": "05b80c2fd6b17849d4b5bb1882a3da4ab32959b2",
+          "message": "add MIT License",
+          "timestamp": "2022-06-01T20:14:32+01:00",
+          "tree_id": "c1879f6fb600eac1ca7502875cd9f073a0a75115",
+          "url": "https://github.com/jamesjarvis/massivelyconcurrentsystems/commit/05b80c2fd6b17849d4b5bb1882a3da4ab32959b2"
+        },
+        "date": 1654110940637,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkWaitManyDepsWithDeps/chans",
+            "value": 20439103,
+            "unit": "ns/op\t       9 B/op\t       0 allocs/op",
+            "extra": "631 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkWaitManyDepsWithDeps/waitgroups",
+            "value": 19122520,
+            "unit": "ns/op\t       9 B/op\t       0 allocs/op",
+            "extra": "630 times\n2 procs"
           }
         ]
       }
