c843301a78d4e31277b7d131b581250af9229a97
James Jarvis <git@jamesjarvis.io>
2022-06-28 01:05:46 +0100

    try to use fewer generics where possible

--- pool/queue_test.go
+++ pool/queue_test.go
@@ -12,21 +12,21 @@ func Test_queue_enqueue(t *testing.T) {
 
 	t.Run("sends successfully", func(t *testing.T) {
 		var wg sync.WaitGroup
-		q := newQueue[int, int](bufferSize, &wg)
+		q := newQueue[int](bufferSize, &wg)
 
-		if err := q.enqueue(context.TODO(), NewUnitOfWork[int, int](10, &wg)); err != nil {
+		if err := q.enqueue(context.TODO(), 10); err != nil {
 			t.Errorf("queue.enqueue() error = %v, wantErr %v", err, nil)
 		}
 	})
 	t.Run("returns context error on deadline exceeded", func(t *testing.T) {
 		var wg sync.WaitGroup
-		q := newQueue[int, int](bufferSize, &wg)
+		q := newQueue[int](bufferSize, &wg)
 
 		deadCtx, cancelFunc := context.WithDeadline(context.TODO(), time.Now().Add(-time.Second))
 		defer cancelFunc()
 		<-deadCtx.Done()
 
-		if err := q.enqueue(deadCtx, NewUnitOfWork[int, int](10, &wg)); err == nil {
+		if err := q.enqueue(deadCtx, 10); err == nil {
 			t.Fatalf("queue.enqueue() error = %v, wantErr %v", err, context.Canceled)
 		}
 	})
@@ -38,17 +38,17 @@ func TestQueueGracefulClose(t *testing.T) {
 
 	t.Run("empty queue closes instantly", func(t *testing.T) {
 		var wg sync.WaitGroup
-		q := newQueue[string, string](bufferSize, &wg)
+		q := newQueue[string](bufferSize, &wg)
 		q.close()
 	})
 	t.Run("queue with items waits for items to leave", func(t *testing.T) {
 		var wg sync.WaitGroup
-		q := newQueue[string, string](bufferSize, &wg)
-		err := q.enqueue(context.TODO(), NewUnitOfWork[string, string]("hello world", &wg))
+		q := newQueue[string](bufferSize, &wg)
+		err := q.enqueue(context.TODO(), "hello world")
 		if err != nil {
 			t.Error(err)
 		}
-		err = q.enqueue(context.TODO(), NewUnitOfWork[string, string]("hello world", &wg))
+		err = q.enqueue(context.TODO(), "hello world")
 		if err != nil {
 			t.Error(err)
 		}
@@ -57,7 +57,7 @@ func TestQueueGracefulClose(t *testing.T) {
 		go func() {
 			for {
 				u, ok := q.dequeue()
-				if !ok || u == nil {
+				if !ok || u == "" {
 					break
 				}
 				itemsRetrieved++
